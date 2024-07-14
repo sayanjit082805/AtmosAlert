f52fde331a835c3ed44b785cce349054e2342836
sayanjit082805 <sayanjitukil2005@gmail.com>
2024-07-14 20:43:59 +0530

    add logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import Weather from "./components/Weather";
+import Title from "./components/Title";
 
 function App() {
   return (
     <>
+      <div className="bg-gradient-to-r from-slate-900 to bg-slate-700">
+        <Title />
+      </div>
       <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-r from-slate-900 to-slate-700">
         <Weather />
       </div>
---
+++ src/components/Title.jsx
@@ -0,0 +1,14 @@
+import logo from "../../public/logo.svg";
+
+export default function Title() {
+  return (
+    <div className="flex">
+      <div>
+        <img className="h-16 w-16 pt-5 float-left" src={logo}></img>
+      </div>
+      <div className="pt-6 bg-gradient-to-r from-slate-400 to-slate-600 bg-clip-text text-transparent font-bold text-[24px]">
+        <h2>AtmosAlert</h2>
+      </div>
+    </div>
+  );
+}
