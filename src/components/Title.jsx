60917544b305d18de6e83312f7ae51c85d1f847b
sayanjit082805 <sayanjitukil2005@gmail.com>
2024-07-15 21:29:43 +0530

    more minor changes

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -4,9 +4,9 @@ export default function Title() {
   return (
     <div className="flex">
       <div>
-        <img className="h-20 w-20 pt-5 float-left" src={logo}></img>
+        <img className="h-20 w-20 pt-4 float-left" src={logo}></img>
       </div>
-      <div className="pt-6 bg-gradient-to-r from-slate-400 to-slate-600 bg-clip-text text-transparent font-bold text-[26px]">
+      <div className="pt-7 bg-gradient-to-r from-slate-400 to-slate-600 bg-clip-text text-transparent font-bold text-[26px]">
         <h2>AtmosAlert</h2>
       </div>
     </div>
